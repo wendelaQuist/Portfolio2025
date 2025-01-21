314a022e0d62280254c2472b9df3723f55d28869
Quist,Wendela W.F <w.quist@student.fontys.nl>
2025-01-21 13:55:38 +0100

    Fixed searchbar menu expand

--- portfolio2025/src/Components/Scripts/Hero.js
+++ portfolio2025/src/Components/Scripts/Hero.js
@@ -12,19 +12,11 @@ const Hero = () => {
     }, [])
 
     return (
-            <div className="relative w-screen h-screen overflow-hidden bg-dark-grey flex flex-col md:flex-row" style={{ paddingTop: "4rem" }}>
-                <div className="flex flex-col justify-center items-start w-full md:w-1/2 px-6 md:px-12 text-pink" style={{ paddingTop: "4rem" }}>
+            <div className="relative w-screen h-screen overflow-hidden bg-dark-grey flex flex-col md:flex-row">
+                <div className="flex flex-col items-start w-full md:w-1/2 px-6 md:px-12 text-pink">
                         <h1 className="text-4xl md:text-6xl font-bold mb-4">Welcome to my portfolio</h1>
-                        <h2 className="text-base md:text-lg">Browse through the projects I've developed over the past <span>{years}</span> years since starting my journey in IT & Media Design</h2>
+                        <h2 className="text-base md:text-lg">Browse through the <a href="#" className="text-decoration-line: underline cursor-pointer">projects</a> I've developed over the past <span>{years}</span> years since starting my journey in IT & Media Design</h2>
                     </div>
-                <div className="flex justify-center items-center w-full md:w-1/2 bg-[#14121A]">
-                    <img
-                        src={leftBlock}
-                        alt="Leftblock"
-                        style={{ paddingTop: "4rem" }}
-                        className="w-[90%] md:w-[80%] lg:w-[60%] h-auto"
-                    />
-                </div>
             </div>
     )
 }
